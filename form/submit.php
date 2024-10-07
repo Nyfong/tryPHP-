e3eba9e93488c60852d3d66d53678a7c141f68f4
Nyfong <litongfong12@gmail.com>
2024-10-07 17:38:18 +0700

    form refresh header

---
+++ form/submit.php
@@ -0,0 +1,19 @@
+<?php
+header("refresh: 1;");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/lib.min.js" integrity="sha512-z/dvZbFZhHCi5/4UoyNwhidEOFbiN93Um+DWoaMZsH6zqifGGo0zA4bUwdf1SgV7KIPMQgiGs+05PNl8ytxweQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <title>First Form</title>
+</head>
+<body>
+
+
+<p>hello </p>
+    
+</body>
+</html>

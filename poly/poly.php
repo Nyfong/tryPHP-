6dc866779aa3efd673ee0c8d40d9551b465003d4
Nyfong <litongfong12@gmail.com>
2024-10-07 08:38:03 +0700

    commit poly

---
+++ poly/poly.php
@@ -0,0 +1,44 @@
+<?php
+
+abstract class Personn{
+    abstract public function greet();
+}
+class English extends Personn {
+    public function greet()
+    {
+        return "hi";
+    }
+}
+class French extends Personn
+{
+    public function greet ()
+    {
+        return " bonjour";
+    }
+}
+class China extends Personn{
+    public function greet()
+    {
+        return "Ni hao";
+    }
+}
+
+$people = [
+    new English(),
+    new French(),
+    new China()
+];
+function greeting($other)
+{
+    foreach ($other as $person )
+    {
+        echo ($person->greet());
+        echo "<br>";
+    }
+
+}
+greeting($people)
+
+// The idea is to create the Absract Class and give definition
+?>
+
